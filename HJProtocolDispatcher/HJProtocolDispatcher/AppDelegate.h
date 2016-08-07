085d377369da2ef61d1bc7698a2b9d39de1ccb6f
庞海礁 <275742376@qq.com>
2016-08-08 00:22:00 +0800

    Add files via upload

---
+++ HJProtocolDispatcher/HJProtocolDispatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJProtocolDispatcher
+//
+//  Created by haijiao on 16/8/8.
+//  Copyright © 2016年 olinone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJProtocolDispatcher/HJProtocolDispatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJProtocolDispatcher
+//
+//  Created by haijiao on 16/8/8.
+//  Copyright © 2016年 olinone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
